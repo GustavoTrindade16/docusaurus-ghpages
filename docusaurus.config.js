2efb24b5cfbed57a55e4b6492906785fae61f4c2
Gustavo-fernandes200 <a85254@ualg.pt>
2023-12-20 21:20:54 +0000

    implementação do titulo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Implementação do York',
+  title: 'Concessionária Virtual',
   tagline: 'Dinossauros são legais',
   favicon: 'img/favicon.ico',
 
