3a90d3b7b368acf4c2088786bea1d675309d5846
GustavoTrindade16 <a85492@ualg.pt>
2023-12-20 20:07:58 +0000

    alteração do do dome da empressa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Gustavo Trindade',
+  title: 'Implementação do York',
   tagline: 'Dinossauros são legais',
   favicon: 'img/favicon.ico',
 
