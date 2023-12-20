fc790bdc66fc2f4ec4a870ce238a78deec50ad83
GustavoTrindade16 <a85492@ualg.pt>
2023-12-20 20:04:34 +0000

    resolver erro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Automóvel Éter',
+  title: 'Gustavo Trindade',
   tagline: 'Dinossauros são legais',
   favicon: 'img/favicon.ico',
 
